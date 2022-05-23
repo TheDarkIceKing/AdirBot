c9bcee9e08aa4e2098c587690109c5dbe2bba4fd
TheDarkIceKing <aytonvanbarreveld2003@gmail.com>
2022-05-24 01:55:52 +0200

    added delay to the restart

--- commands/Developer/restartBot.js
+++ commands/Developer/restartBot.js
@@ -2,13 +2,16 @@ const Discord = require("discord.js")
 
 
 module.exports.run = async (bot, message, args) => {
-    process.exit();
+    message.channel.send({content: "restarting bot"})
+    setTimeout(function () {
+        process.exit();
+    }, 5000);
 }
 
 module.exports.config = {
     name: "restart",
     description: "restarts the bot",
-    aliases: [],
-    usage: "restart",
+    aliases: ["!kill"],
+    usage: "restar",
     permission: "DEVELOPER"
 }
